c3085fa4d270446ccb7d4ff5e774c341e8a7a01c
Tony <gorez.tony@gmail.com>
2017-11-14 18:16:45 +0100

    Step12

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,10 +5,10 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>My <span class="highlight">DJ</span> is me !</h1>
-        <div class="App">
+        <h1>My <span className="highlight">DJ</span> is me !</h1>
+        <div className="App">
           // Add a SearchBar component
-          <div class="App-playlist">
+          <div className="App-playlist">
             // Add a SearchResults component
             // Add a Playlist component
           </div>
